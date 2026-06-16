d0ac4969c5826dd57cb59647c4b21a1eb1c4ff11
akfalah <akfalah1107@gmail.com>
2026-06-16 16:44:36 +0700

    feat: update default server port

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import { logger } from './applications/logger';
 import { server } from './applications/server';
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8888
 
 server.listen(PORT, () => {
   logger.info(`Listening on port ${PORT}`);
